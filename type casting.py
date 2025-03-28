9781448d2615eca92ca2cd76ae1e20967d6c2f06
s0urlemon <dheekshida.balaji@gmail.com>
2025-03-28 19:51:20 +0530

    Add files via upload

---
+++ type casting.py
@@ -0,0 +1,19 @@
+name="Penguin"
+age=15
+is_student=True
+weight=38.5
+print("Name:",name)
+print("Data type of name is",type(name))
+print("Age:",age)
+print("Data type of age is",type(age))
+print("is_student:",is_student)
+print("Data type of is_student is",type(is_student))
+print("weight:",weight)
+print("Data type of weight is",type(weight))
+print("\nAfter Type Casting....")
+age=str(age)
+print(weight)
+print("Data Type of age is",type(age))
+weight=int(weight)
+print(weight)
+print("Data Type of weight is",type(weight))
