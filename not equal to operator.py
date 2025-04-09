fa0f827e1c6f675d24b1cf772caecfde50ad0fee
s0urlemon <dheekshida.balaji@gmail.com>
2025-04-09 18:59:05 +0530

    Add files via upload

---
+++ not equal to operator.py
@@ -0,0 +1,27 @@
+a=10
+b=12
+c=12
+
+print(a!=b)
+print(b!=c)
+
+p="hi"
+q="hello"
+
+if a!=b:
+    print(a,"and",b,"are different")
+
+m=27
+n=64
+
+if (m==9)!=(n==96):
+    print(m,"and",n,"are different")
+
+print("enter a number to find out whether it is even or odd")
+z=int(input("enter a number: "))
+
+if z%2!=0:
+    print(z,"is an odd number")
+    
+else:
+    print(z,"is an even number")
