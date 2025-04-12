178bd6d4d558494b31e47af2af2931e4fa4502b4
s0urlemon <dheekshida.balaji@gmail.com>
2025-04-12 14:08:02 +0530

    Add files via upload

---
+++ age check.py
@@ -0,0 +1,13 @@
+age=int(input("Enter you age: "))
+
+if age>10:
+    print("you are above 10 years old")
+
+    if age>=18:
+        print("you are an adult.")
+
+    else:
+        print("you are not yet an adult")
+
+else:
+    print("you are very young")
