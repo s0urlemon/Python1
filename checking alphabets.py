bddcf67caab30f4d7233dcfcd68617891c486cae
s0urlemon <dheekshida.balaji@gmail.com>
2025-04-16 17:34:19 +0530

    Add files via upload

---
+++ checking alphabets.py
@@ -0,0 +1,7 @@
+import string
+char=input("Enter a character to identify whether it a alphabet or not: ")
+if char>="a" and char<="z":
+    print("The character which you entered is an alphabet")
+
+else:
+    print("The character which you entered is not an alphabet")
