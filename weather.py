9b54dd58b8a1caab887fae0f0fd8d2eccf7d09a2
s0urlemon <dheekshida.balaji@gmail.com>
2025-04-16 16:40:23 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,7 @@
+temp=int(input("Enter the current temperature in your area(in celsius): "))
+
+if temp>=30:
+    print("It is quite hot.So wear light,summer clothes suited for summer to avoid discomfort.")
+
+else:
+    print("It is quite cold.So wear layered,warm clothes suited for winter to avoid discomfort.")
