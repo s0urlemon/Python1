fdfe569fe5ef89d45668ff5e10f1c987b8d47ffd
s0urlemon <dheekshida.balaji@gmail.com>
2025-04-25 19:25:11 +0530

    Add files via upload

---
+++ pass statement.py
@@ -0,0 +1,10 @@
+for x in range(35):
+    if x%20==0:
+        print("twist")
+    elif x%15==0:
+        pass
+    elif x%5==0:
+        print("fizz")
+    elif x%3==0:
+        print("buzz")
+    else:print(x)
