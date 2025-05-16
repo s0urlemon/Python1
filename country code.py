761d2ec2d13122e09dda2dd41d88c9611703f1e8
s0urlemon <dheekshida.balaji@gmail.com>
2025-05-16 13:41:14 +0530

    Add files via upload

---
+++ country code.py
@@ -0,0 +1,11 @@
+code={
+    "India":"0091",
+    "Australia":"0025",
+    "Nepal":"00977"
+}
+
+print("Country code for India is:")
+print(code.get("India","Not found"))
+
+print("Country code for Japan is:")
+print(code.get("Japan","Not found"))
