87fe3dc82873fd8606629f82fb5d87253f3eb090
s0urlemon <dheekshida.balaji@gmail.com>
2025-10-10 19:50:56 +0530

    Add files via upload

--- display and resize.py
+++ display and resize.py
@@ -1,7 +1,7 @@
 import cv2
 image=cv2.imread('example.jpg')
 cv2.namedWindow('Loaded Image',cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Loaded Image',800,500)
+cv2.resizeWindow('Loaded Image',900,600)
 cv2.imshow('Loaded Image',image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
